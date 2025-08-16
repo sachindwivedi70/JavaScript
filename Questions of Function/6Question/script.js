316a88e05db9e70b701af415ef13b9bb5bc2d9c5
sachindwivedi70 <sachindwivedi7084@gmail.com>
2025-08-16 07:52:20 +0530

    convert function in to pure function

---
+++ Questions of Function/6Question/script.js
@@ -0,0 +1,11 @@
+// convert the above function into a pure function..
+// let total=0;
+// function addTotal(num ){
+//     total+=num;
+// }
+
+let total=0;
+function addTotal(num ){
+    let newtotal=total;
+    newtotal+=num;
+}
