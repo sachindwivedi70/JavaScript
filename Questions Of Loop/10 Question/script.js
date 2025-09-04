07fa5a3158d10688d6aeaeb520d2a7ba048ffdf5
sachindwivedi70 <sachindwivedi7084@gmail.com>
2025-09-04 10:44:50 +0530

    stop at first multiple of 7

---
+++ Questions Of Loop/10 Question/script.js
@@ -0,0 +1,12 @@
+//stop at first multiple of 7 
+// write a loop from 1 to 100 that:
+// prints each number
+// stops completely when if finds the first number divisivle by 7
+
+for ( let i=1;i<101;i++){
+    if(i%7==0 ){
+      break;
+        
+    }
+    console.log(i);
+}
